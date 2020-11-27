4d9ad69fc164fc201d745bf93b0c01b125d49bf4
jinkim3 <jin.m.kim@yale.edu>
2020-11-27 02:57:20 -0500

    .

---
+++ R/cohen_d.R
@@ -0,0 +1,51 @@
+#' Cohen's d with confidence interval
+#'
+#' Calculates Cohen's d using the 'effsize' package v0.8.1 by
+#' Torchiano (2020) <https://github.com/mtorchiano/effsize/>
+#'
+#' @param sample_1 a vector of values in the first of two samples
+#' @param sample_2 a vector of values in the second of two samples
+#' @param data a data object (a data frame or a data.table)
+#' @param iv_name name of the independent variable
+#' @param dv_name name of the dependent variable
+#' @param ci_range range of the confidence interval for Cohen's d
+#' (default = 0.95)
+#' @examples
+#' cohen_d(1:10, 3:12)
+#' cohen_d(data = mtcars, iv_name = "vs", dv_name = "mpg", ci_range = 0.99)
+#'
+#' @export
+cohen_d <- function(
+  sample_1 = NULL, sample_2 = NULL,
+  data = NULL, iv_name = NULL, dv_name = NULL,
+  ci_range = 0.95) {
+  if (!is.null(sample_1) & !is.null(sample_2)) {
+    if (is.numeric(sample_1) & is.numeric(sample_2)) {
+      df <- data.frame(
+        "iv" = c(
+          rep("sample_1", length(sample_1)),
+          rep("sample_2", length(sample_2))),
+        "dv" = c(sample_1, sample_2))
+    } else {
+      stop(paste0(
+        "Please make sure that both of the vectors, sample_1 ",
+        "and sample_2 are numeric vectors."))
+    }
+  }
+  # if data object is provided
+  if (!is.null(data) & !is.null(iv_name) & !is.null(dv_name)) {
+    if (length(unique(data[[iv_name]])) != 2) {
+      stop(paste0(
+        "The independent variable has ",
+        length(unique(data[[iv_name]])), " levels.\n",
+        "Cohen's d can be calculated when there are exactly 2 levels."))
+    } else {
+      df <- data.frame(
+        "iv" = data[[iv_name]],
+        "dv" = data[[dv_name]])
+    }
+  }
+  # convert iv to factor
+  df$iv <- factor(df$iv)
+  effsize::cohen.d(formula = dv ~ iv, data = df, conf.level = ci_range)
+}
--- R/cohen_d_from_cohen_textbook.R
+++ R/cohen_d_from_cohen_textbook.R
@@ -3,15 +3,39 @@
 #' Calculates Cohen's d as described in Jacob Cohen's textbook (1988),
 #' Statistical Power Analysis for the Behavioral Sciences, 2nd Edition
 #'
-#' @param sample_1_as_vector a vector of values in the first of two samples
-#' @param sample_2_as_vector a vector of values in the second of two samples
+#' @param sample_1 a vector of values in the first of two samples
+#' @param sample_2 a vector of values in the second of two samples
+#' @param data a data object (a data frame or a data.table)
+#' @param iv_name name of the independent variable
+#' @param dv_name name of the dependent variable
 #' @examples
 #' cohen_d_from_cohen_textbook(1:10, 3:12)
+#' cohen_d_from_cohen_textbook(
+#' data = mtcars, iv_name = "vs", dv_name = "mpg")
 #' @export
 cohen_d_from_cohen_textbook <- function(
-  sample_1_as_vector, sample_2_as_vector) {
-  s1 <- sample_1_as_vector[!is.na(sample_1_as_vector)]
-  s2 <- sample_2_as_vector[!is.na(sample_2_as_vector)]
+  sample_1 = NULL, sample_2 = NULL,
+  data = NULL, iv_name = NULL, dv_name = NULL) {
+  if (!is.null(sample_1) & !is.null(sample_2)) {
+    s1 <- sample_1[!is.na(sample_1)]
+    s2 <- sample_2[!is.na(sample_2)]
+  }
+  # if data object is provided
+  if (!is.null(data) & !is.null(iv_name) & !is.null(dv_name)) {
+    if (length(unique(data[[iv_name]])) != 2) {
+      stop(paste0(
+        "The independent variable has ",
+        length(unique(data[[iv_name]])), " levels.\n",
+        "Cohen's d can be calculated when there are exactly 2 levels."))
+    } else {
+      s1 <- subset(
+        data, get(iv_name) == sort(unique(data[[iv_name]]))[1])
+      s2 <- subset(
+        data, get(iv_name) == sort(unique(data[[iv_name]]))[2])
+      s1 <- s1[!is.na(s2)]
+      s2 <- s2[!is.na(s2)]
+    }
+  }
   output <- (mean(s1) - mean(s2)) / sqrt(
     (sum((s1 - mean(s1))^2) + sum((s2 - mean(s2))^2)) /
       (length(s1) + length(s2) - 2))
