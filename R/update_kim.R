69f044b4bc31e9ee15304b07a3ca7a80520233f1
jinkim3 <jin.m.kim@yale.edu>
2020-12-02 01:03:42 -0500

    .

--- R/update_kim.R
+++ R/update_kim.R
@@ -13,9 +13,14 @@
 #' @export
 #' @import devtools
 update_kim <- function(source = "github") {
+  # unload the package kim
+  while ("package:kim" %in% search()) {
+    detach("package:kim", unload = TRUE, character.only = TRUE)
+  }
   if (source == "github") {
     # if source is github
     kim::prep("devtools")
     devtools::install_github("jinkim3/kim")
   }
+  kim::prep("kim")
 }
