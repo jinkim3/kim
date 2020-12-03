1460eb005cf0323f7dbf95d5c1f690b8d0061f50
jinkim3 <jin.m.kim@yale.edu>
2020-12-03 11:44:08 -0500

    21

--- R/onAttach.R
+++ R/onAttach.R
@@ -10,19 +10,20 @@
     readLines(url),
     warning = function(w) {"no_result"},
     error = function(e) {"no_result"})
+  update_suggestion <- paste0(
+    "This may not be the most recent version of the package available",
+    " on github. If you run into errors using the package, please",
+    " consider updating the package to the most recent version on",
+    " github by running 'update_kim()'"
+  )
   if (identical(github_pkg_desc, "no_result")) {
     if (is.na(installed_pkg_version)) {
       startup_message <- paste0(
-        "Attached: kim (version unknown)\nThis may not be the",
-        " most recent version of the package available on github.",
-        "\nIf you run into errors using the package, please consider",
-        " updating the package by running 'update_kim()'")
+        "Attached package: kim (version unknown). ", update_suggestion)
     } else {
       startup_message <- paste0(
-        "Attached: kim ", installed_pkg_version, "\nThis may not be the",
-        " most recent version available on github.",
-        "\nIf you run into errors using the package, please consider",
-        " updating the package by running 'update_kim()'")
+        "Attached package: kim ", installed_pkg_version,
+        ". ", update_suggestion)
     }
   } else {
     # get the version number of github version
@@ -35,31 +36,36 @@
       error = function(e) {NA})
     # message to print
     if (is.na(compare_version_result)) {
-      startup_message <- paste0(
-        "Attached: kim (version unknown)\nThis may not be the",
-        " most recent version of the package available on github.",
-        "\nIf you run into errors using the package, please consider",
-        " updating the package by running 'update_kim()'")
+      if (is.na(installed_pkg_version)) {
+        startup_message <- paste0(
+          "Attached package: kim (version unknown). ", update_suggestion)
+      } else {
+        startup_message <- paste0(
+          "Attached package: kim ", installed_pkg_version,
+          ". ", update_suggestion)
+      }
     } else if (compare_version_result == -1) {
       startup_message <- paste0(
-        "\nA more recent version of the package 'kim' is available ",
-        "on github:\n",
-        "Currently installed version: ", installed_pkg_version,"\n",
-        "Available newer version:     ", github_pkg_version,
-        "\nIf you run into errors using the package, please ",
-        "consider updating by running 'update_kim()'")
+        "A more recent version of the package 'kim' is available ",
+        "on github.\n",
+        "Currently installed version: ", installed_pkg_version, "\n",
+        "Newer version on github:     ", github_pkg_version, "\n",
+        "If you run into errors using the package, please",
+        " consider updating the package to the most recent version on",
+        " github by running 'update_kim()'")
     } else if (compare_version_result == 0) {
       startup_message <- paste0(
-        "Attached: kim ", installed_pkg_version, "(the most recent version ",
-        "available on github")
+        "Attached package: kim ", installed_pkg_version, " (same ",
+        "version as the most recent version available on github.)")
     } else if (compare_version_result == 1) {
       startup_message <- paste0(
-        "You are running a newer, developer version (v",
-        installed_pkg_version, ") of the ",
-        "package 'kim' that may not be stable.\n",
-        "If you run into errors using the package, please consider ",
-        "downgrading the package by running 'update_kim()'"
-      )
+        "You are running a newer, but possibly unstable version of ",
+        "the package 'kim'.\n",
+        "Currently installed version: ", installed_pkg_version, "\n",
+        "Older version on github:     ", github_pkg_version, "\n",
+        "If you run into errors using the package, please",
+        " consider downgrading the package to the older version on",
+        " github by running 'update_kim()'")
     }
   }
   packageStartupMessage(startup_message)
