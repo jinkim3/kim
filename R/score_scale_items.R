d3ca72e17697e1f35ea82119b7d36d20abf23efd
jinkim3 <jin.m.kim@yale.edu>
2021-03-15 23:38:58 -0400

    135

--- R/score_scale_items.R
+++ R/score_scale_items.R
@@ -6,11 +6,9 @@
 #' @param items list of scale items (i.e., list of vectors of ratings)
 #' to code normally (as opposed to reverse coding).
 #' @param reverse list of scale items to reverse code.
-#' @param mean logical. If \code{mean = TRUE}, mean of the scale items
-#' will be calculated (default = TRUE).
-#' @param sum logical. Input for the argument \code{mean} takes precedence.
-#' If \code{mean = FALSE, sum = TRUE}, sum of the scale items will
-#' be calculated (default = FALSE).
+#' @param operation if \code{operation = "mean"}, mean of the scale items
+#' will be calculated; if \code{operation = "sum"}, sum of the scale items
+#' will be calculated (default = "mean").
 #' @param na_summary logical. If \code{na_summary = TRUE} a summary of
 #' NA values will be printed; if \code{na_summary = FALSE} the summary
 #' will not be printed (default = TRUE).
@@ -22,23 +20,17 @@
 #' score_scale_items(items = list(1:5, rep(3, 5)),
 #' reverse = list(rep(5, 5)), reverse_code_minuend = 6)
 #' score_scale_items(items = list(c(1, 1), c(1, 5)),
-#' reverse = list(c(5, 3)), reverse_code_minuend = 6)
+#' reverse = list(c(5, 3)), reverse_code_minuend = 6, na_summary = FALSE)
+#' score_scale_items(items = list(c(1, 1), c(1, 5)),
+#' reverse = list(c(5, 1)), reverse_code_minuend = 6, operation = "sum")
 #' @export
 #' @import data.table
 score_scale_items <- function(
   items = NULL,
   reverse = NULL,
-  mean = TRUE,
-  sum = FALSE,
+  operation = "mean",
   na_summary = TRUE,
   reverse_code_minuend = NULL) {
-  # mean takes precedence over sum
-  if (mean == TRUE & sum == TRUE) {
-    sum <- FALSE
-    message(paste0(
-      'Although both the "mean" and "sum" arguments were set to be ',
-      "TRUE, only the mean will be calculated."))
-  }
   # deal with argument inputs
   if (!is.null(reverse)) {
     if (is.null(reverse_code_minuend)) {
@@ -102,11 +94,11 @@ score_scale_items <- function(
   # data table of item ratings
   ratings_dt <- setDT(items_after_rev_coding)[]
   # calculate mean
-  if (mean == TRUE) {
+  if (operation == "mean") {
     output <- rowMeans(ratings_dt, na.rm = TRUE)
   }
   # calculate sum
-  if (sum == TRUE) {
+  if (operation == "sum") {
     output <- rowSums(ratings_dt, na.rm = TRUE)
   }
   # final output
