80e3796d5c7d28e090bc2abeeaae39f22af36920
jinkim3 <jin.m.kim@yale.edu>
2022-01-21 18:33:04 -0500

    136

--- R/und.R
+++ R/und.R
@@ -8,10 +8,15 @@
 #' @examples
 #' # correlation
 #' und(corr_text, x = 1:5, y = c(1, 2, 2, 2, 3))
+#' und(mean_center, 1:10)
+#' scale(1:10, scale = TRUE)
+#' v3 <- 1:10
+#' und(mean_center, v3)
 #' @export
 #' @import data.table
 und <- function(fn, ...) {
   # list of arguments entered
+  # al stands for argument list
   al <- as.list(match.call(expand.dots = TRUE))
   # change function name to a character
   fn <- as.character(al$fn)
@@ -20,6 +25,7 @@ und <- function(fn, ...) {
   # environment for evaluating language
   focal_environment <- new.env(parent = parent.frame())
   # evaluate languages
+  # ae stands for arguments evaluated
   ae <- lapply(al, eval, envir = focal_environment)
   # corr text
   if (fn == "corr_text") {
@@ -47,6 +53,36 @@ und <- function(fn, ...) {
       stop("Please provide arguments for the function `cor.test`")
     }
   }
+  # mean center, standardize, z_score
+  if (fn == "mean_center") {
+    output <- scale(x = x, scale = FALSE)
+    # if the output is a vector, return the vector
+    if (dim(output)[2] == 1) {
+      output <- as.vector(output)
+    }
+    return(output)
+  }
+  if (fn %in% c("standardize", "z_score")) {
+    output <- scale(x = x, scale = TRUE)
+    # if the output is a vector, return the vector
+    if (dim(output)[2] == 1) {
+      output <- as.vector(output)
+    }
+    return(output)
+  }
+  # # confirm that only one vector is entered
+  # if (length(ae) == 1) {
+  #   x <- ae[[1]]
+  # } else if ("x" %in% names(ae)) {
+  #   x <- ae[["x"]]
+  # } else {
+  #   stop(paste0(
+  #     "There must be only one input, or the input must be entered ",
+  #     "as follows: x = [input]"))
+  # }
+  # if (is.numeric(x) == FALSE) {
+  #   stop("Please enter a numeric vector as an input.")
+  # }
   # if nothing was returned by this point, the function must have been
   # incorrectly entered
   stop(paste0(
