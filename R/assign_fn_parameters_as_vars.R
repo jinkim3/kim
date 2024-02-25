74743c88a044ae98f338877916adec88e5cc5b51
unknown <jin kim>
2024-02-25 02:04:29 -0500

    1

--- R/assign_fn_parameters_as_vars.R
+++ R/assign_fn_parameters_as_vars.R
@@ -33,60 +33,39 @@ assign_fn_parameters_as_vars <- function(
   function_as_string <- deparse(substitute(fun))
   print(paste0("ls list: ", ls()))
   print(function_as_string)
-  message("5555")
-  # search the function within the default environment
-  if (exists(function_as_string) && is.function(get(function_as_string))) {
+  message("66")
+  # search the function within the global environment
+  if (exists(function_as_string, where = .GlobalEnv, inherits = FALSE)) {
     print(1)
-    print(exists(function_as_string, where = .GlobalEnv, inherits = FALSE))
+  } else if (exists(
+    function_as_string, where = baseenv(), inherits = FALSE)) {
     print(2)
-    print(exists(function_as_string, where = baseenv(), inherits = FALSE))
-
-
-    # Get the list of attached packages' namespaces
-    attached_packages <- search()
-
-    # Initialize a variable to store the search result
-    found_in_packages <- FALSE
-
-    # Loop through each attached package
-    for (pkg in attached_packages) {
-      # Attempt to convert the package name into a namespace, and then check if the object exists within it
-      if (grepl("package:", pkg)) {
-        namespace <- sub("package:", "", pkg)
-        if (exists(function_as_string, where = asNamespace(namespace), inherits = FALSE)) {
-          cat(sprintf("Found '%s' in package: %s\n", function_as_string, namespace))
-          found_in_packages <- TRUE
-          break
-        }
+  } else {
+    print(3)
+    # loop through the search path for attached packages and namespaces
+    pkg_containing_the_function <- NULL
+    for (env in search()) {
+      if (exists(
+        function_as_string, where = as.environment(env),
+        inherits = FALSE)) {
+        pkg_containing_the_function <- env
+        print(pkg_containing_the_function)
+        break
       }
     }
-
-    # If the object wasn't found in any attached package
-    if (!found_in_packages) {
-      cat(sprintf("'%s' was not found in any attached packages.\n", function_as_string))
-    }
-
-
-
-
-    print(paste0("ls list: ", ls()))
-    print(utils::getAnywhere(function_as_string))
-    # environment(function_as_string)
-    parameters <- formals(args(match.fun(function_as_string)))
-  } else {
-    # search the function within the package kim
-    if (exists(
-      function_as_string, where = asNamespace("kim"), inherits = FALSE)) {
-      print(3)
-      function_from_kim <- utils::getFromNamespace(
-        function_as_string, "kim")
-      parameters <- formals(args(match.fun(function_from_kim)))
-      kim::pm(
-        "The function `", function_as_string, "` was found ",
-        "within Package 'kim'.")
-
-    }
+    print(pkg_containing_the_function)
   }
+  # else if (exists(
+  #   function_as_string, where = asNamespace("kim"), inherits = FALSE)) {
+  #   # search the function within the package kim
+  #   print(4)
+  #   function_from_kim <- utils::getFromNamespace(
+  #     function_as_string, "kim")
+  #   parameters <- formals(args(match.fun(function_from_kim)))
+  #   kim::pm(
+  #     "The function `", function_as_string, "` was found ",
+  #     "within Package 'kim'.")
+  # }
   # manually assign each element of the list to the global environment
   for (name in names(parameters)) {
     assign(name, parameters[[name]], envir = .GlobalEnv)
